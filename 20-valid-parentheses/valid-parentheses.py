8439bebfca073df94745b148d432c85ac76644c8
alina-cai <117325741+alina-cai@users.noreply.github.com>
2024-03-03 19:36:27 -0800

    Time: 33 ms (74.64%) | Memory: 16.6 MB (44.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dict = {')': '(', '}': '{', ']': '['}
+        pars = {')': '(', '}': '{', ']': '['}
         stack = []
 
         for c in s:
             if c in "({[":
                 stack.append(c)
                 continue
-            elif not stack or stack[-1] != dict[c]:
+            elif not stack or stack[-1] != pars[c]:
                 return False
 
             stack.pop()
@@ -16,4 +16,3 @@ class Solution:
 
 # time: O(n)
 # space: O(n)
-        
